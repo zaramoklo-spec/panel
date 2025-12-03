b8a8b5f568f09da8169af936c6fb12fb03dd649e
angelcode119 <mpouanfar@gmail.com>
2025-12-03 12:21:57 +0330

    Fixed foregroundServiceType crash issue

--- lib/core/utils/popup_helper.dart
+++ lib/core/utils/popup_helper.dart
@@ -1,16 +1,31 @@
+import 'dart:js_interop';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:url_launcher/url_launcher.dart';
 
-void openDevicePopup(String deviceId) async {
+@JS('window.open')
+external JSObject? _windowOpen(String url, String target, String features);
+
+void openDevicePopup(String deviceId) {
   if (!kIsWeb) return;
   
   final currentUrl = Uri.base.toString().split('#')[0];
-  final deviceUrl = Uri.parse('$currentUrl#/device/$deviceId');
+  final deviceUrl = '$currentUrl#/device/$deviceId';
   
-  if (await canLaunchUrl(deviceUrl)) {
-    await launchUrl(
-      deviceUrl,
-      mode: LaunchMode.externalApplication,
-    );
-  }
+  const width = 414;
+  const height = 896;
+  
+  @JS('window.screen.width')
+  external int get _screenWidth;
+  
+  @JS('window.screen.height')
+  external int get _screenHeight;
+  
+  final screenWidth = _screenWidth;
+  final screenHeight = _screenHeight;
+  
+  final left = ((screenWidth - width) / 2).round();
+  final top = ((screenHeight - height) / 2).round();
+  
+  final features = 'width=$width,height=$height,left=$left,top=$top,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,status=no';
+  
+  _windowOpen(deviceUrl, '_blank', features);
 }
